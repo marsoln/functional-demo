b662186891a76aa26c0b938b3d5e552323fc85d5
zhaobiao <zhaobiao@intv.com.cn>
2016-07-31 13:57:11 +0800

    我脑子疼

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 > 所有功能,只在chrome/firefox dev edition的 latest version 验证过
 > `npm i & npm i webpack -g & webpack` 然后打开根目录 `home.html` 预览即可
 
-## 菜单入口
+## 示例内容
 
-1. [列表拖拽选择](/demos/dragSelect/index.html)
-2. [正多边形绘制](/demos/polygonCalculator/index.html)
+1. 列表拖拽选择
+2. 正多边形绘制
--- demos/polygonCalculator/entry.js
+++ demos/polygonCalculator/entry.js
@@ -4,9 +4,49 @@ let $reset = btnReset
 let $angleAmount = angleAmount
 let $polygon = polygon
 let $container = container
-let $values = valueArr
-let regenerate = () => {
-    $polygon.setAttribute('points', new PolygonCalculator($container.clientWidth / 2, $container.clientHeight / 2, +$angleAmount.value, 10, 40).generatePointsPath(...$values.value.split(',')))
+let _forEach = [].forEach
+let _reduce = [].reduce
+// amount changed
+$angleAmount.addEventListener('change', function () {
+    let _newAmount = this.value
+    amountDisplay.innerText = _newAmount
+    generateThePointRangeBars(_newAmount)
+})
+
+barsContainer.addEventListener('change', (ev) => {
+    let $target = ev.target || ev.srcElement
+    if (~$target.className.indexOf('whatever')) {
+        reDraw()
+    }
+}, true)
+
+let generateThePointRangeBars = (amount) => {
+    while (barsContainer.childElementCount)
+        _forEach.call(barsContainer.children, c => c.remove())
+    while (amount-- > 0) {
+        let item = document.createElement('li')
+        let input = document.createElement('input')
+        input.className = 'whatever'
+        input.setAttribute('type', 'number')
+        input.setAttribute('value', 3)
+        input.setAttribute('min', 1)
+        input.setAttribute('max', 10)
+        item.appendChild(input)
+        barsContainer.appendChild(item)
+    }
+    reDraw()
 }
 
-$reset.addEventListener('click', regenerate)
+let reDraw = () => {
+    try {
+        $polygon.setAttribute('points',
+            new PolygonCalculator($container.clientWidth / 2, $container.clientHeight / 2, +$angleAmount.value, 10, 40)
+                .generatePointsPath(..._reduce.call(document.getElementsByClassName('whatever'), (p, n) => {
+                    p[p.length] = n.value
+                    return p
+                }, [])))
+    } catch (e) {
+        alert(e.message)
+    }
+}
+generateThePointRangeBars(5)
